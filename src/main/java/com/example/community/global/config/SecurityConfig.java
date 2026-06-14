855190d266fb6efaad0d3b9cb491861e8ad39111
Youngjin <156926628+youn9jin@users.noreply.github.com>
2026-06-15 00:17:12 +0900

    Feat: 배포 설정

--- src/main/java/com/example/community/global/config/SecurityConfig.java
+++ src/main/java/com/example/community/global/config/SecurityConfig.java
@@ -74,7 +74,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("https://reeve.o-r.kr"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
