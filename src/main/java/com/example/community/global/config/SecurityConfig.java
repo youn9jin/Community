7608e43898f363ef4ad4e4690a300834b20e06c7
Youngjin <156926628+youn9jin@users.noreply.github.com>
2026-08-02 21:42:31 +0900

    fix: k8s FE CORS origin 허용하도록 수정

--- src/main/java/com/example/community/global/config/SecurityConfig.java
+++ src/main/java/com/example/community/global/config/SecurityConfig.java
@@ -75,7 +75,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://reeve.o-r.kr"));
+        config.setAllowedOrigins(List.of(
+                "https://reeve.o-r.kr",
+                "https://k8s.reeve.o-r.kr"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
